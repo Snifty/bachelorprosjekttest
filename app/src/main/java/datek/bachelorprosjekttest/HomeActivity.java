0d5bcd30521836c4e008a4832e59405a6f8a2dde
snifty <aleksanderne1@gmail.com>
2017-01-25 16:02:37 +0100

    test 123123131231

--- app/src/main/java/datek/bachelorprosjekttest/HomeActivity.java
+++ app/src/main/java/datek/bachelorprosjekttest/HomeActivity.java
@@ -56,6 +56,8 @@ public class HomeActivity extends Activity {
                 return false;
             }
 
+            
+
             try {
                 mLedGpio.setValue(!mLedGpio.getValue());
             } catch (IOException e) {
